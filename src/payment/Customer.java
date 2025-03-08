bd1dde66293f7bbe76b4bd22b93993010377d9f9
goli <shirafkanzeynab63@gmail.com>
2025-03-08 11:38:35 +0330

    add base body of classes

--- src/payment/Customer.java
+++ src/payment/Customer.java
@@ -1,4 +1,26 @@
 package payment;
 
-public class Customer {
+import java.util.ArrayList;
+
+public abstract class Customer {
+String name;
+ArrayList<String>paymentHistory;
+
+public Customer(String name){
+    this.name = name;
+}
+public abstract void displayCustomerInfo();
+public void makePayment(PaymentStrategy paymentStrategy, double amount){
+
+}
+public void showPaymentHistory(){
+
+}
+class RegularCustomer{
+
+}
+class PremiumCustomer{
+
+}
+
 }
